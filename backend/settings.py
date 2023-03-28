90696473d8aa41c3694a06c07908fc52c7c7e39f
tony-f22 <efrengkou@gmail.com>
2023-03-28 10:56:21 +0300

    Added heroku app to ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://antonis-26032023.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'antonis-26032023.herokuapp.com']
 
 # Application definition
 
